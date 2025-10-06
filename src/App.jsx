1dad310b903e97fe0f700a54f94c588921501269
kamalrajms <kamalrajindtn@gmail.com>
2025-10-06 15:05:43 +0530

    introduction

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeting from "./Greeting";
+import Header from "./component/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <h1> hello world</h1>
+      <Greeting />
+    </>
+  );
+}
